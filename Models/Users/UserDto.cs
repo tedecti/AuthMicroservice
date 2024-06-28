ebf446fc3ce4f429b43fd4c2f55f8bc13a71b502
tedecti <wolftheguy08@gmail.com>
2024-06-28 22:48:58 +0500

    fixed typo

--- Models/Users/UserDto.cs
+++ Models/Users/UserDto.cs
@@ -3,5 +3,5 @@ namespace AuthMicroservice.Models.Users;
 public class UserDto
 {
     public string Email { get; set; }
-    public string Passord { get; set; }
+    public string Password { get; set; }
 }
