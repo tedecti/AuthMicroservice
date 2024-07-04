ca35db5ec42d043417cf04e1caf54a9707fcc2aa
tedecti <wolftheguy08@gmail.com>
2024-07-04 14:38:17 +0500

    fixed "nul token" problem + some catch in jwt generating

--- Repositories/AuthRepository.cs
+++ Repositories/AuthRepository.cs
@@ -12,31 +12,33 @@ namespace AuthMicroservice.Repositories;
 public class AuthRepository : IAuthRepository
 {
     private readonly AuthDbContext _context;
-
-    public AuthRepository(AuthDbContext context)
+    private readonly IConfiguration _configuration;
+    public AuthRepository(AuthDbContext context, IConfiguration configuration)
     {
         _context = context;
+        _configuration = configuration;
     }
 
     public async Task<User> Register(User user)
     {
+        var hashedPassword = BCrypt.Net.BCrypt.HashPassword(user.Password, 12);
         var newUser = new User
         {
             Id = Guid.NewGuid(),
             Email = user.Email,
             Name = user.Name,
-            Password = BCrypt.Net.BCrypt.HashPassword(user.Password),
+            Password = hashedPassword,
             UserType = user.UserType
         };
-        _context.Users.Add(user);
+        _context.Users.Add(newUser);
         await _context.SaveChangesAsync();
         return newUser;
     }
 
-    public async Task<string?> Login(UserDto userDto)
-    {
+    public async Task<string> Login(UserDto userDto)
+    { 
         var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == userDto.Email);
-        if (user == null || BCrypt.Net.BCrypt.Verify(userDto.Passord, user.Password))
+        if (user == null || string.IsNullOrEmpty(userDto.Password) || !BCrypt.Net.BCrypt.Verify(userDto.Password, user.Password))
         {
             return null;
         }
@@ -45,17 +47,32 @@ public class AuthRepository : IAuthRepository
         return token;
     }
 
-    private string GenerateJwtToken(User user)
+    public string GenerateJwtToken(User user)
     {
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes("your_secret_key_here");
-        var tokenDescriptor = new SecurityTokenDescriptor
+        try
         {
-            Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()), new Claim("role", user.UserType) }),
-            Expires = DateTime.UtcNow.AddDays(7),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-        };
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-        return tokenHandler.WriteToken(token);
+            var jsonKey = _configuration.GetValue<string>("SecretKey");
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(jsonKey);
+            if (string.IsNullOrEmpty(jsonKey))
+            {
+                throw new InvalidOperationException("SecretKey is missing or empty in configuration.");
+            }
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                    { new Claim("id", user.Id.ToString()), new Claim("role", user.UserType) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+        catch (Exception e)
+        {
+            throw new InvalidOperationException(e.ToString());
+        }
     }
 }
