e7273772ea7808435fe61c71afe343b7b211169f
tedecti <wolftheguy08@gmail.com>
2024-07-04 21:10:29 +0500

    add validation for model

--- Models/Users/User.cs
+++ Models/Users/User.cs
@@ -1,10 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AuthMicroservice.Models.Users;
 
 public class User
 {
+    [Required]
     public Guid Id { get; set; }
+    [Required]
+    [MaxLength(50)]
     public string Name { get; set; }
+    [Required]
+    [EmailAddress]
+    [MaxLength(100)]
     public string Email { get; set; }
+    [Required]
+    [MinLength(8)]
     public string Password { get; set; }
+    [Required]
+    [AllowedValues("Seller", "Customer")]
     public string UserType { get; set; }
 }
